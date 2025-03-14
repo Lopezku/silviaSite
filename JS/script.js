c64539973676d64cad102e4ec1ba9449b8f1300b
lopez <jerome_lopez@hotmail.Com>
2025-03-14 22:23:19 +0100

    add first section text - design menu - modify position in menu

--- JS/script.js
+++ JS/script.js
@@ -5,18 +5,32 @@ function changerLangue(langue) {
       if (!response.ok) {
         throw new Error(`Erreur HTTP ${response.status} - Fichier introuvable`);
       }
-      console.log("::::::::::::::::::::", response);
       return response.json();
     })
     .then((data) => {
-      console.log("🚀 ~ .:::: then ~ data:", data);
       const formations = document.querySelectorAll(".cursus-elt-text"); // Récupère tous les blocs formations
       const titreFormation = document.getElementById("titre-formation");
       titreFormation.innerText = data.titreFormation;
-      console.log("🚀 ~ :::: .then ~ titreFormation:", titreFormation);
+      const section1Title = document.getElementById("section1Title"); // Sélectionne toutes les descriptions
+      section1Title.innerText = data.section1Titre;
+
+      const sectionTitleElements = document.querySelectorAll(
+        ".pres_container .pres_block .pres_elt .pres_title h3"
+      ); // Sélectionne tous les titres
+      const sectionDescriptionElements = document.querySelectorAll(
+        ".pres_container .pres_block .pres_elt .pres_content div"
+      ); // Sélectionne toutes les descriptions
+
+      data.portrait.forEach((item, index) => {
+        if (sectionTitleElements[index]) {
+          sectionTitleElements[index].textContent = item.titre; // Utilisation de "titre" au lieu de "title"
+        }
+        if (sectionDescriptionElements[index]) {
+          sectionDescriptionElements[index].textContent = item.description;
+        }
+      });
 
       const navItems = document.querySelectorAll(".navbar-nav .nav-item a"); // Sélectionne tous les <a> dans les <li>
-
       data.menuEntries.forEach((menu, index) => {
         if (navItems[index]) {
           // Vérifie que l'élément existe
@@ -25,10 +39,6 @@ function changerLangue(langue) {
       });
       // Parcours chaque formation dans le JSON et l'affiche dans l'ordre
       data.formations.forEach((formation, index) => {
-        console.log(
-          "🚀 ~ :::::: data.formations.forEach ~ formation:",
-          formation
-        );
         if (formations[index]) {
           // Vérifie que l'élément HTML existe
           formations[index].querySelector(".cursus-title").textContent =
