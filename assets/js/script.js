abba665cbbb206475cb591669f4788f2bb30e91e
lopez <jerome_lopez@hotmail.Com>
2025-03-17 22:03:25 +0100

    add design for navbar and behaviour on mobile - design blue for dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,31 +16,64 @@ window.onload = function () {
   }
 };
 
+document.addEventListener("DOMContentLoaded", function () {
+  let navLinks = document.querySelectorAll(".nav-link.menu_anchor");
+  let navbarToggler = document.querySelector(".navbar-toggler");
+  let navbarCollapse = document.querySelector("#navbarNav");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      // Vérifier si le menu est ouvert
+      if (navbarCollapse.classList.contains("show")) {
+        navbarToggler.click(); // Simule un clic sur le bouton burger pour fermer
+      }
+    });
+  });
+});
+
 // Sélection du bouton et du corps du document
 const themeToggleButton = document.getElementById("themeToggle");
 const body = document.body;
 const navBarSite = document.getElementById("navBarSite");
-const navItems = document.getElementById("navItem");
+document.querySelectorAll(".menu_anchor").forEach((link) => {
+  link.addEventListener("click", function () {
+    // Supprime la classe "active" de tous les liens
+    document
+      .querySelectorAll(".menu_anchor")
+      .forEach((el) => el.classList.remove("active"));
+
+    // Ajoute la classe "active" au lien cliqué
+    this.classList.add("active");
+  });
+});
 
 // Ajouter un événement de clic
 themeToggleButton.addEventListener("click", () => {
   // Basculer entre les classes 'dark-mode' et 'light-mode'
   body.classList.toggle("dark-mode");
   body.classList.toggle("light-mode");
-  navBarSite.classList.toggle("bg-dark");
+  navBarSite.classList.toggle("bg-primary");
   navBarSite.classList.toggle("bg-light");
   let navbar = document.getElementById("navBarSite");
-
+  let navLinks = document.querySelectorAll("#navItem .nav-link");
   if (document.body.classList.contains("dark-mode")) {
     navbar.classList.remove("navbar-light", "bg-light");
-    navbar.classList.add("navbar-dark", "bg-dark");
+    navbar.classList.add("navbar-dark", "bg-primary");
+    navLinks.forEach((link) => {
+      link.classList.remove("navbarLight");
+      link.classList.add("navbarDark");
+    });
   } else {
-    navbar.classList.remove("navbar-dark", "bg-dark");
+    navbar.classList.remove("navbar-dark", "bg-primary");
     navbar.classList.add("navbar-light", "bg-light");
+    navLinks.forEach((link) => {
+      link.classList.remove("navbarDark");
+      link.classList.add("navbarLight");
+    });
   }
 
   // Mettre à jour le texte du bouton
   themeToggleButton.textContent = body.classList.contains("dark-mode")
-    ? "Mode Clair"
-    : "Mode Sombre";
+    ? `🌗 Clair`
+    : `🌗 Bleu`;
 });
